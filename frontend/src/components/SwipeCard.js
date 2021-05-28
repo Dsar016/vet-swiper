94dc30dfeda3c22054be4144fc5b6c1ff0495aaa
Harpreet Singh <hsingh9600@gmail.com>
2021-05-28 21:35:05 +1200

    add swipe

--- frontend/src/components/SwipeCard.js
+++ frontend/src/components/SwipeCard.js
@@ -1,43 +1,70 @@
 import CardLocation from "./CardLocation";
 import "./SwipeCard.css";
-import React from "react";
-import CloseIcon from '@material-ui/icons/Close';
-import DoneIcon from '@material-ui/icons/Done';
+import React, { useState } from "react";
+import CloseIcon from "@material-ui/icons/Close";
+import DoneIcon from "@material-ui/icons/Done";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import StyledRating from "@material-ui/lab/Rating";
+import PetsIcon from "@material-ui/icons/Pets";
+import TinderCard from "react-tinder-card";
 
 const useStyles = makeStyles({
   root: {
-    width: "100%"
+    width: "100%",
   },
 });
 
+const onSwipe = (direction) => {
+  console.log("You swiped: " + direction);
+};
+
+const onCardLeftScreen = (myIdentifier) => {
+  console.log(myIdentifier + " left the screen");
+};
+
 export default function SwipeCard(props) {
-  console.log(props);
   const classes = useStyles();
-  console.log(props.location);
-  console.log("!!!!!" + JSON.stringify(props.data));
+
   return (
     <div className="swipe-card-container">
-      <Card className={classes.root}>
-        <CardActionArea>
-          <CardLocation location={props.location} />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              {props.data["firstName"]} {props.data["lastName"]}
-            </Typography>
-            <Typography variant="body1" color="textSecondary" component="p">
-              <div>Email: {props.data["email"]}</div>
-              <div>Email: {props.data["email"]}</div>
-              <div>Email: {props.data["email"]}</div>
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
+      <TinderCard
+        onSwipe={onSwipe}
+        onCardLeftScreen={() => onCardLeftScreen("fooBar")}
+        preventSwipe={["right", "left"]}
+        style={{ width: "100%" }}
+        className="swipe-card"
+      >
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardLocation location={props.location} />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {props.data["firstName"]} {props.data["lastName"]}
+              </Typography>
+              <StyledRating
+                name="customized-color"
+                defaultValue={2}
+                getLabelText={(value) =>
+                  `${value} Heart${value !== 1 ? "s" : ""}`
+                }
+                precision={0.5}
+                icon={<PetsIcon fontSize="inherit" />}
+                readOnly
+              />
+              <Typography variant="body1" color="textSecondary" component="p">
+                <div>Email: {props.data["email"]}</div>
+                <div>Email: {props.data["email"]}</div>
+                <div>Email: {props.data["email"]}</div>
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </TinderCard>
       <div className="swipe-card-button">
         <Button
           size="large"
@@ -45,6 +72,7 @@ export default function SwipeCard(props) {
           color="primary"
           className={classes.button}
           startIcon={<DoneIcon />}
+          // onClick={() => swipe("left")}
         >
           Yeah
         </Button>
@@ -54,6 +82,7 @@ export default function SwipeCard(props) {
           color="secondary"
           className={classes.button}
           startIcon={<CloseIcon />}
+          // onClick={() => swipe("right")}
         >
           Naaa
         </Button>
